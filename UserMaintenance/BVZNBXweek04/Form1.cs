065d62470736796b617b76d54b96e85ae61b7e8c
Lenovo <Lenovo@DESKTOP-LB2OVF4>
2021-10-07 13:55:14 +0200

    Adatok betöltése, Excel eszközök behivatkozása

--- UserMaintenance/BVZNBXweek04/Form1.cs
+++ UserMaintenance/BVZNBXweek04/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace BVZNBXweek04
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            List<Flat> Flats = context.Flats.ToList();
         }
     }
 }
